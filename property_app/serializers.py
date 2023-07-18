6c61dd8f88f07531e62c751d869d949d153db5d0
ragnar <zackmwangi998@gmail.com>
2023-07-18 16:54:53 +0300

    format

--- property_app/serializers.py
+++ property_app/serializers.py
@@ -2,10 +2,22 @@ from rest_framework import serializers
 from .models import *
 
 
+# class PropertySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Property
+#         fields = "__all__"
+
+
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = "__all__"
+
+    def to_internal_value(self, data):
+        # Convert the 'name' field to title case
+        data["name"] = data.get("name", "").title()
+        return super().to_internal_value(data)
+
 
 
 class RoomSerializer(serializers.ModelSerializer):
--- property_app/views.py
+++ property_app/views.py
@@ -22,21 +22,13 @@ def client_property_list(request, client_id):
 @api_view(["POST"])
 def create_property(request):
     serializer = PropertySerializer(data=request.data)
-    try:
-        serializer.is_valid(raise_exception=True)
+    if serializer.is_valid():
         serializer.save(added_by=request.user)
         return Response(
             {"message": "Property created successfully", "data": serializer.data},
             status=status.HTTP_201_CREATED,
         )
-    except serializers.ValidationError as e:
-        # Check if the error is due to uniqueness constraint violation
-        if "name" in e.detail and "unique" in e.detail["name"][0].lower():
-            error_message = {"name": ["A property with this name already exists."]}
-        else:
-            error_message = e.detail
-
-        return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
@@ -157,12 +149,10 @@ def delete_room(request, pk):
         return Response({"message": "Room not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
-# delete-all
 
+# delete-all
 
 @api_view(["DELETE"])
 def delete_all_rooms(request):
     Room.objects.all().delete()
-    return Response(
-        {"message": "All rooms deleted successfully"}, status=status.HTTP_204_NO_CONTENT
-    )
+    return Response({"message": "All rooms deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
