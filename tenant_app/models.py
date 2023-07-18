7381397c1961fa4c6a4b8e61d30b67b6cad2e8f9
ragnar <zackmwangi998@gmail.com>
2023-07-18 18:19:04 +0300

    format

--- tenant_app/models.py
+++ tenant_app/models.py
@@ -4,8 +4,8 @@ from client_app.models import Client
 from property_app.models import Property, Room
 from django.conf import settings
 
-class Tenant(models.Model):
 
+class Tenant(models.Model):
     STATUS_CHOICES = (
         ("Active", "Active"),
         ("Inactive", "Inactive"),
@@ -23,19 +23,18 @@ class Tenant(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
-    deposit_amount_paid = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    deposit_amount_paid = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
     deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.fullname}"
-    
 
     def save(self, *args, **kwargs):
         # Convert the name and alternative names to title case before saving
         self.fullname = self.fullname.title()
         self.alternative_names = self.alternative_names.title()
         super(Tenant, self).save(*args, **kwargs)
-
-
