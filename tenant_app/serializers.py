0e55dc44ed2f963d16bf7fc6e55b6858e9c82b77
ragnar <zackmwangi998@gmail.com>
2023-07-18 17:40:15 +0300

    format

--- tenant_app/serializers.py
+++ tenant_app/serializers.py
@@ -7,16 +7,17 @@ class TenantSerializer(serializers.ModelSerializer):
         model = Tenant
         fields = "__all__"
 
+        def to_internal_value(self, data):
+            # Convert the 'name' field to title case
+            data["fullname"] = data.get("fullname", "").title()
+            return super().to_internal_value(data)
+
+
 
 class TenantRoomTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenant
         fields = ["room"]
-
-        def to_internal_value(self, data):
-            # Convert the 'name' field to title case
-            data["fullname"] = data.get("fullname", "").title()
-            return super().to_internal_value(data)
 
 
 class TenantUpdateSerializer(serializers.ModelSerializer):
