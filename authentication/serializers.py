8ff4e541cc89cf64cefe7e9d8adc4d604b8b2af3
ragnar <zackmwangi998@gmail.com>
2023-08-03 14:44:49 +0300

    done with issues

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -55,4 +55,4 @@ class UserSerializer(serializers.ModelSerializer):
 class UserSerializerrrrr(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'user_status']
