4ceff0172f9f7a59573f587d7ce50db57663c46e
ragnar <zackmwangi998@gmail.com>
2023-08-06 19:48:36 +0300

    done

--- authentication/urls.py
+++ authentication/urls.py
@@ -11,12 +11,10 @@ urlpatterns = [
     # path('verify-email/<uidb64>/<token>', VerifyEmail.as_view(), name='activate'),
     path("users/<int:pk>", views.user_detail),
     path("user_list/<int:client_id>", views.user_list),
-
-    path('users/update/<int:pk>', views.user_partial_update, name='user-partial-update'),
-
-    # user_mapping  
-    path('prop-assign/get_all', get_user_mappings_all, name='get_user_mappings_all'),
-    path('prop-assign/get/<int:user_id>', get_user_mappings, name='get_user_mappings'),
-    path('prop-assign', create_user_mapping, name='create_user_mapping'),
-    path('prop-assign/del/<int:pk>', delete_user_mapping, name='delete_user_mapping'),
+    path("users/update/<int:pk>", user_update, name="user-partial-update"),
+    # user_mapping
+    path("prop-assign/get_all", get_user_mappings_all, name="get_user_mappings_all"),
+    path("prop-assign/get/<int:user_id>", get_user_mappings, name="get_user_mappings"),
+    path("prop-assign", create_user_mapping, name="create_user_mapping"),
+    path("prop-assign/del/<int:pk>", delete_user_mapping, name="delete_user_mapping"),
 ]
