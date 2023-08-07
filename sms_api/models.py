2a1536f23ed7a8b2df0b2e603bdac4a65a805a09
ragnar <zackmwangi998@gmail.com>
2023-08-07 14:05:56 +0300

    q

--- sms_api/models.py
+++ sms_api/models.py
@@ -5,10 +5,11 @@ from client_app.models import Client
 class SmsCredential(models.Model):
     api = models.TextField()
     username = models.CharField(max_length=100)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, unique=True)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE, primary_key=True)
 
     class Meta:
         unique_together = ["api", "client"]
 
     def __str__(self):
         return str(self.client)
+
