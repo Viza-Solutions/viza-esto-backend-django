2802f4fa18d175f011b165ed977d9819028c7574
ragnar <zackmwangi998@gmail.com>
2023-08-17 13:56:56 +0300

    kk

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(User)
+
+# admin.site.register(User)
+@admin.register(User)
+class User(ImportExportModelAdmin):
+    list_display = ("id", "email")
